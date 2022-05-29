e88ac5d278c240be2a25a6cb52b03fb4686737b4
esma-yigit <75517864+esma-yigit@users.noreply.github.com>
2022-05-29 14:21:12 +0300

    a little fix

--- controllers/address.go
+++ controllers/address.go
@@ -4,15 +4,15 @@ import "github.com/gin-gonic/gin"
 
 //login//signup
 
-func AddAddress() gin.HandlerFunc {
-
-}
-func EditHomeAddress() gin.HandlerFunc {
-
-}
-func EditWorkAddress() gin.HandlerFunc {
-
-}
+//func AddAddress() gin.HandlerFunc {
+//
+//}
+//func EditHomeAddress() gin.HandlerFunc {
+//
+//}
+//func EditWorkAddress() gin.HandlerFunc {
+//
+//}
 func DeleteAddress() gin.HandlerFunc {
 
 }
