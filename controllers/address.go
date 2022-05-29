82dbb9855941ce0425a99c5c7f637587b2c37bf8
esma-yigit <75517864+esma-yigit@users.noreply.github.com>
2022-05-29 15:44:44 +0300

    GetItemFromCart controller done

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/esma-yigit/partial-ecommerce/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"net/http"
+	"time"
+)
 
 //login//signup
 
@@ -14,5 +22,30 @@ import "github.com/gin-gonic/gin"
 //
 //}
 func DeleteAddress() gin.HandlerFunc {
-
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error": "invalid search index"})
+			c.Abort()
+			return
+		}
+		addresses := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal server error")
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wrong command")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Successfully deleted.")
+	}
 }
--- controllers/card.go
+++ controllers/card.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 	"github.com/esma-yigit/partial-ecommerce/database"
+	"github.com/esma-yigit/partial-ecommerce/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
@@ -132,5 +134,42 @@ return func(c *gin.Context) {
 
 
 func GetItemFromCart() gin.HandlerFunc {
+return func(c *gin.Context) {
+	user_id:=c.Query("id")
+	if user_id=="" {
+		c.Header("Content-Type","application/json")
+		c.JSON(http.StatusNotFound,gin.H{"Error":"Invalid id"})
+		c.Abort()
+		return
+	}
+	usert_id,_:=primitive.ObjectIDFromHex(user_id)
+	var ctx,cancel=context.WithTimeout(context.Background(),100*time.Second)
+	defer cancel()
+	var filledcart models.User
+	err:=UserCollection.FindOne(ctx,bson.D{primitive.E{Key: "_id",Value: usert_id}}).Decode(&filledcart)
+	if err!=nil{
+		log.Println(err)
+		c.IndentedJSON(500,"not found")
+	}
+	filter_match:=bson.D{{Key: "$match",Value: bson.D{primitive.E{Key: "_id",Value: usert_id}}}}
+	unwind:=bson.D{{Key: "$unwind",Value: bson.D{primitive.E{Key:"path",Value: "$usercart"}}}}
+	groping:=bson.D{{Key: "$group",Value: bson.D{primitive.E{Key: "_id",Value: "$_id"},{Key: "total",Value: primitive.D{primitive.E{Key:"$sum",Value: "$usercart.price"}}}}}}
+    pointcursor,err := UserCollection.Aggregate(ctx,mongo.Pipeline{filter_match,unwind,groping})
+	if err != nil {
+		log.Println(err)
 
+	}
+	var listing []bson.M
+	if err=pointcursor.All(ctx,&listing);err!=nil {
+		log.Panicln(err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+	for _, json := range listing {
+		c.IndentedJSON(200,json["total"])
+		c.IndentedJSON(200,filledcart.UserCart)
+	}
+	ctx.Done()
+
+
+}
 }
